edfa13ef1d61b827bbb22050dd37b910c18538b3
synsyst <jesper.hededam@gmail.com>
2024-05-15 19:58:22 +0200

    added apikey secret test

--- TestWebApp.py
+++ TestWebApp.py
@@ -5,3 +5,6 @@ number = st.slider("pick a number", 0, 50)
 
 st.write(f"you picked as your {number}")
 
+apikey = st.secrets["myAPIKey"]
+st.write(f"also the api key is {apikey}, though it's fake and it just tests the secrets functionality of streamlit")
+
